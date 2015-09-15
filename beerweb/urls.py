9618ec0b321fd174a303502ab37dd594c1653d4a
Lichura <lichun88@gmail.com>
2015-09-14 21:56:07 -0300

    Se agrego el archivo base.html para usar de plantilla

--- beerweb/urls.py
+++ beerweb/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', 'cervezas.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^cervezas/', include('cervezas.urls')),
+    #url(r'^cervezas/', include('cervezas.urls')),
+    url(r'^cervezas/', 'cervezas.views.cervezas_list', name='cervezas'),
+    url(r'^contact/', 'cervezas.views.contact', name='contact'),
+    url(r'^gallery/', 'cervezas.views.gallery', name='gallery'),
+	url(r'^services/', 'cervezas.views.services', name='services'),
 ]
--- cervezas/views.py
+++ cervezas/views.py
@@ -8,3 +8,12 @@ def cervezas_list(request):
 
 def home(request):
 	return render(request, 'cervezas/home.html', {})
+	
+def contact(request):
+	return render(request, 'cervezas/contact.html',{})
+	
+def gallery(request):
+	return render(request, 'cervezas/gallery.html',{})
+	
+def services(request):
+	return render(request, 'cervezas/services.html',{})
