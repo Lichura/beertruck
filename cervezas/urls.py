a6665f620f46dfb1c249886763ecf5584e5588eb
Lichura <lichun88@gmail.com>
2015-09-29 23:07:19 -0300

    se corrigio el about us

--- cervezas/urls.py
+++ cervezas/urls.py
@@ -2,7 +2,5 @@ from django.conf.urls import include,url
 from . import views
 
 urlpatterns= [
-	url(r'^cervezas$', views.cervezas_list),
 	url(r'', views.home),
-	url(r'^contact$', views.contact),
 ]
--- cervezas/views.py
+++ cervezas/views.py
@@ -2,18 +2,7 @@ from django.shortcuts import render
 from cervezas import models as cerv_models
 
 # Create your views here.
-def cervezas_list(request):
-	cervezas = cerv_models.Cervezas.objects.all()
-	return render(request, 'cervezas/cervezas_list.html', {'cervezas': cervezas})
-
 def home(request):
-	return render(request, 'cervezas/home.html', {})
-	
-def contact(request):
-	return render(request, 'cervezas/contact.html',{})
-	
-def gallery(request):
-	return render(request, 'cervezas/gallery.html',{})
-	
-def services(request):
-	return render(request, 'cervezas/services.html',{})
+	marcas = cerv_models.Marcas.objects.all()
+	cervezas = cerv_models.Cervezas.objects.all()
+	return render(request, 'cervezas/home.html', {'cervezas': cervezas, 'marcas': marcas})
