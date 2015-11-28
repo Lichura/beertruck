8c4879a05e97d56d5655a79819982c3b157e5d7d
Lichura <lichun88@gmail.com>
2015-11-28 18:06:29 -0300

    se modifico la forma de carga de las cervezas

--- beerweb/settings.py
+++ beerweb/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+#from settings_secret import *
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -103,11 +104,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'sendgrid_username'
-EMAIL_HOST_PASSWORD = 'sendgrid_password'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' 
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'lgurman'
+EMAIL_HOST_PASSWORD = 'Mailing2015'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-TIMEOUT = 100
